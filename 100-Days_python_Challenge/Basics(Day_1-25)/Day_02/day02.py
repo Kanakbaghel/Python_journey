4c11921deebcf20b7fac7f39808c602296f04d0c
Kanakbaghel <kanak.bgl704@gmail.com>
2025-09-10 15:38:35 +0530

    Update

--- 100-Days_python_Challenge/Basics(Day_1-25)/Day_02/day02.py
+++ 100-Days_python_Challenge/Basics(Day_1-25)/Day_02/day02.py
@@ -8,8 +8,37 @@ if True:
 x = 10 # Variable can be numeric
 name = "Aakansha" #variable can be categorical
 
+# To execute(print) the variable
+print(x)
+print(name)
+print("Hello, World!") 
+
 #Basic Data Types
+
+## Integer
 age = 35
 temperature = -5
-print(type(age))
+print(type(age)) # we use type Function to know the variable type
 print(type(temperature))
+
+## Float
+price = 2334.234
+pi = 3.14159
+print(type(price))
+print(type(pi))
+
+## String
+name = "Vanshika" 
+a = "34"
+multiline = """This is
+multi-line 
+string."""
+print(type(name))
+print(type(a))
+print(type(multiline))
+
+## Boolean
+changed = True
+unchanged = False
+print(type(changed))
+print(type(unchanged))
