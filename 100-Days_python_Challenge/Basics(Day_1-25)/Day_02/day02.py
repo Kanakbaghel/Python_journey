207738f4158473b95b7df4df7ee40c1390dca020
Kanakbaghel <kanak.bgl704@gmail.com>
2025-09-10 12:57:22 +0530

    update

---
+++ 100-Days_python_Challenge/Basics(Day_1-25)/Day_02/day02.py
@@ -0,0 +1,15 @@
+# This is a comment.
+
+# Using Indentation.
+if True:
+    print("This is indented.")
+
+# Variables in python
+x = 10 # Variable can be numeric
+name = "Aakansha" #variable can be categorical
+
+#Basic Data Types
+age = 35
+temperature = -5
+print(type(age))
+print(type(temperature))
